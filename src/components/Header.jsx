fe9c27fe9f14ccec1690d9e313bf36d2e9ed87d5
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-22 04:41:46 +0100

    add navbar links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,31 @@
+import { NavLink } from "react-router-dom";
+
 function Header() {
   return (
     <header className="flex justify-between items-center p-8">
-      <h1 className="text-2xl font-bold"></h1>
-      <nav className="flex gap-8 justify-between items-center"></nav>
+      <h1 className="text-2xl font-bold"> </h1>
+      <nav className="flex gap-8 justify-between items-center">
+        <NavLink
+          className={({ isActive }) => (isActive ? "text-blue-500" : "")}
+          to="/">
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "text-blue-500" : "")}
+          to="/shop">
+          Shop
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "text-blue-500" : "")}
+          to="/about">
+          About
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "text-blue-500" : "")}
+          to="/contact">
+          Contact
+        </NavLink>
+      </nav>
       <div className="flex gap-4 items-center"></div>
     </header>
   );
