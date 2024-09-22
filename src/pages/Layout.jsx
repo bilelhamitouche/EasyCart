2c2135ba1e33796fe46193b735edca8add3bb409
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-22 04:26:01 +0100

    create footer

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -6,6 +6,14 @@ function Layout() {
         <nav className="flex gap-8 justify-between items-center"></nav>
         <div className="flex gap-4 items-center"></div>
       </header>
+      <footer className="flex flex-col justify-center items-center p-8">
+        <Link to="/home">Home</Link>
+        <Link to="/shop">Shop</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <div className="flex gap-2 justify-center items-center"></div>
+        <p className="text-sm">Copyright &copy; Bilel Hamitouche 2024.</p>
+      </footer>
     </div>
   );
 }
