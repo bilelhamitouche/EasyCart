b6f29cf2f25e03803656e120831125a8fb67a3e2
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-22 05:20:31 +0100

    add missing imports and react-icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { BsCart3 } from "react-icons/bs";
 
 function Header() {
   return (
     <header className="flex justify-between items-center p-8">
-      <h1 className="text-2xl font-bold"> </h1>
+      <h1 className="text-2xl font-bold">EasyCart</h1>
       <nav className="flex gap-8 justify-between items-center">
         <NavLink
           className={({ isActive }) => (isActive ? "text-blue-500" : "")}
@@ -26,7 +27,13 @@ function Header() {
           Contact
         </NavLink>
       </nav>
-      <div className="flex gap-4 items-center"></div>
+      <div className="flex gap-4 items-center">
+        <Link
+          to="/cart"
+          className="p-2 rounded-full hover:bg-gray-100">
+          <BsCart3 className="text-xl font-bold" />
+        </Link>
+      </div>
     </header>
   );
 }
