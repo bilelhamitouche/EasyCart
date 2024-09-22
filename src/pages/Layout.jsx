7071145a3071102704a5e2fed8a0e33803b71863
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-22 04:25:51 +0100

    create header

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,11 @@
 function Layout() {
   return (
     <div className="grid h-full grid-rows-layout">
+      <header className="flex justify-between items-center p-8">
+        <h1 className="text-2xl font-bold"></h1>
+        <nav className="flex gap-8 justify-between items-center"></nav>
+        <div className="flex gap-4 items-center"></div>
+      </header>
     </div>
   );
 }
