916c421c06ea070dace2b2e56f08333f7c0207cc
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-23 12:18:49 +0100

    add Feature Card component

---
+++ src/components/FeatureCard.jsx
@@ -0,0 +1,10 @@
+function FeatureCard({ feature, icon }) {
+  return (
+    <div className="flex flex-col gap-4 justify-center items-center py-8 rounded-lg border-2 border-blue-700">
+      {icon}
+      <h3 className="text-xl font-bold text-blue-700">{feature}</h3>
+    </div>
+  );
+}
+
+export default FeatureCard;
