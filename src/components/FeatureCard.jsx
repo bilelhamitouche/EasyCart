4d57f0516549a1fcd3dcfd1e5abc9511d5e1b7b7
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-23 12:37:15 +0100

    add hover effects for card components

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,6 +1,6 @@
 function FeatureCard({ feature, icon }) {
   return (
-    <div className="flex flex-col gap-4 justify-center items-center py-8 rounded-lg border-2 border-blue-700">
+    <div className="flex flex-col gap-4 justify-center items-center py-8 rounded-lg border-2 border-blue-700 transition-transform hover:scale-105">
       {icon}
       <h3 className="text-xl font-bold text-black">{feature}</h3>
     </div>
