e9fd65fc433620b403106f761d9f43c26628a4b7
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-25 22:25:27 +0100

    add loader to get data from the api

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,7 +2,16 @@ import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
+export async function layoutLoader() {
+  const response = await fetch("https://fakestoreapi.com/products", {
+    mode: "cors",
+  });
+  if (!response.ok) throw new Error("Unable to fetch products");
+  const json = await response.json();
+  return json;
+}
 function Layout() {
+  const products = useLoaderData();
   return (
     <div className="grid h-screen grid-rows-layout">
       <Header />
