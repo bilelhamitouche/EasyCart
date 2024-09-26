59de46916381d47d2ef0172e5065931f9b8517f9
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-27 00:49:17 +0100

    remove console log

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,7 +3,6 @@ import ProductCard from "../components/ProductCard";
 
 function Shop() {
   const products = useOutletContext();
-  console.log(products);
   return (
     <main className="grid grid-cols-4 gap-8 p-8 mx-auto lg:container">
       {products.map((product) => (
