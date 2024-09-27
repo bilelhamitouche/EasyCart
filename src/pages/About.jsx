b0e254cf353d8cba7da44b3a6157ed46e1075774
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-27 03:43:24 +0100

    add newsletter

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 import aboutImage from "../assets/about-image.jpg";
 
 function About() {
   return (
-    <main className="p-8 mx-auto h-full lg:container">
-      <section className="flex gap-8 justify-center items-center">
+    <section className="flex flex-col gap-32 justify-center items-center p-8 mx-auto h-full lg:container">
+      <div className="flex gap-8 justify-center items-center">
         <div className="flex flex-col gap-6 justify-center w-1/2">
           <h2 className="text-4xl font-bold">About Us</h2>
           <p className="text-lg font-gray-500">
@@ -26,8 +27,24 @@ function About() {
           alt="about image"
           className="object-center w-1/2 rounded-lg shadow-sm shadow-gray-600"
         />
-      </section>
-    </main>
+      </div>
+      <div className="flex flex-col gap-8 justify-center items-center">
+        <h1 className="text-3xl font-bold">Subscribe to our newsletter</h1>
+        <div className="flex justify-center items-center">
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter email address"
+            className="py-3 px-4 text-lg rounded-l-lg border border-gray-500 outline-none"
+          />
+          <Link
+            to="/email"
+            className="py-3 px-4 text-lg text-white bg-black rounded-r-lg border border-black hover:opacity-80">
+            Submit
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 }
 
