d503da34948796d6a3bcc3407173950e62843a25
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-27 05:30:06 +0100

    add loading spinner component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-12 h-12 rounded-full border-4 ease-in-out animate-spin border-y-blue-500 border-x-transparent"></div>
+    </div>
+  );
+}
+
+export default Loading;
