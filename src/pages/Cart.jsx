d5289e50f2de037cd8e7bf5b42306f53f64754eb
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-28 17:36:28 +0100

    add total price

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,13 +3,12 @@ import { Link } from "react-router-dom";
 import { CartContext } from "../contexts/CartContext";
 
 function Cart() {
-  const { cart, removeFromCart, emptyCart } = useContext(CartContext);
-  const total = cart.length;
+  const { cart, removeFromCart, emptyCart, total } = useContext(CartContext);
   return (
     <section className="flex flex-col gap-16 justify-center items-center p-8 mx-auto h-full lg:container">
       {cart.length === 0 ? (
         <div className="flex flex-col gap-8 justify-center items-center">
-          <span className="text-3xl font-semibold">Your cart is empty</span>
+          <span className="text-3xl font-medium">Your cart is empty</span>
           <Link
             to="/shop"
             className="py-2 px-5 text-lg font-bold text-white bg-blue-700 rounded-lg hover:bg-blue-500">
