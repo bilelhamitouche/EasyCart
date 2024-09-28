45f00525fe602f6626e15e28caf03d4a229e1d45
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-28 17:36:15 +0100

    add cart badge for added items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import { Link, NavLink } from "react-router-dom";
 import { BsCart3 } from "react-icons/bs";
 import { MdSunny } from "react-icons/md";
+import { useContext } from "react";
+import { CartContext } from "../contexts/CartContext";
 
 function Header() {
+  const { cart } = useContext(CartContext);
   return (
     <header className="flex justify-between items-center p-8">
       <Link
@@ -35,7 +38,11 @@ function Header() {
       <div className="flex gap-4 items-center">
         <Link
           to="/cart"
-          className="p-2 rounded-full hover:bg-gray-100">
+          className="relative p-2 rounded-full hover:bg-gray-100">
+          <span
+            className={`w-4 bottom-5 bg-red-500 text-center text-white left-5 text-[11px] h-4 rounded-full ${cart.length === 0 ? "hidden" : "absolute"}`}>
+            {cart.length}
+          </span>
           <BsCart3 className="text-xl font-bold" />
         </Link>
         <button className="p-2 rounded-full hover:bg-gray-100">
